e16d80e6d62524cbaf4fbba9ebba4645788b635a
Bjørnar Hvidsten <devenania@proton.me>
2025-07-10 10:43:10 +0200

    Export more components from lib/ui "root"

--- lib/ui/index.ts
+++ lib/ui/index.ts
@@ -1,29 +1,3 @@
-export { Opener } from './base/Opener'
-
-export {
-  Button,
-  buttonKinds,
-  buttonSizes,
-  type ButtonKind,
-  type ButtonProps,
-} from './buttons/Button'
-
-export {
-  IconButton,
-  iconButtonContainer,
-  iconButtonIconSizeRecord,
-  iconButtonKinds,
-  iconButtonSizeRecord,
-  iconButtonSizes,
-  type IconButtonKind,
-  type IconButtonProps,
-  type IconButtonSize,
-} from './buttons/IconButton'
-
-export { OpenMenuButton } from './buttons/OpenMenuButton'
-
-export { ShyTextButton } from './buttons/ShyTextButton'
-
 export { GlobalStyle } from './css/GlobalStyle'
 
 export {
@@ -36,41 +10,64 @@ export {
   type StackProps,
 } from './css/stack'
 
-export { EditIcon } from './icons/EditIcon'
-
-export { MoonIcon } from './icons/MoonIcon'
-
-export { TrashBinIcon } from './icons/TrashBinIcon'
-
-export { Checkbox } from './inputs/Checkbox/Checkbox'
-
-export { Menu, type MenuView } from './menu'
-
-export { MenuOption, type MenuOptionProps } from './menu/MenuOption'
-
-export {
-  PopoverMenu,
-  type PopoverMenuProps,
-  type RenderOpenerProps,
-} from './menu/PopoverMenu'
-
-export { ConfirmationModal } from './modal/ConfirmationModal'
-
-export { ExpandablePanel } from './panel/ExpandablePanel'
-
 export { createPersistentStateHook } from './state/createPersistentStateHook'
-
 export { createPersistentStateManager } from './state/createPersistentStateManager'
-
 export { LocalStorage } from './state/LocalStorage'
-
 export { TemporaryStorage } from './state/TemporaryStorage'
-
-export { Text, text, type TextColor, type TextProps } from './text'
 
 export {
   DarkLightThemeProvider,
   useThemePreference,
 } from './theme/DarkLightThemeProvider'
 
+export { Opener } from './base/Opener'
+
 export { themePreferences, type ThemePreference } from './theme/ThemePreference'
+
+export {
+  Button,
+  buttonKinds,
+  buttonSizes,
+  type ButtonKind,
+  type ButtonProps,
+} from './buttons/Button'
+
+export { Checkbox } from './inputs/Checkbox/Checkbox'
+
+export {
+  IconButton,
+  iconButtonContainer,
+  iconButtonIconSizeRecord,
+  iconButtonKinds,
+  iconButtonSizeRecord,
+  iconButtonSizes,
+  type IconButtonKind,
+  type IconButtonProps,
+  type IconButtonSize,
+} from './buttons/IconButton'
+export { OpenMenuButton } from './buttons/OpenMenuButton'
+export { ShyTextButton } from './buttons/ShyTextButton'
+
+export { ConfirmationModal } from './modal/ConfirmationModal'
+export { ExpandablePanel } from './panel/ExpandablePanel'
+export { Modal } from './modal'
+
+export { Menu, type MenuView } from './menu'
+export { MenuOption, type MenuOptionProps } from './menu/MenuOption'
+export {
+  PopoverMenu,
+  type PopoverMenuProps,
+  type RenderOpenerProps,
+} from './menu/PopoverMenu'
+
+export { Spinner } from './loaders/Spinner'
+export { Text, text, type TextColor, type TextProps } from './text'
+export { Tooltip } from './tooltips/Tooltip'
+
+export { DownloadIcon } from './icons/DownloadIcon'
+export { EditIcon } from './icons/EditIcon'
+export { InfoIcon } from './icons/InfoIcon'
+export { ListIcon } from './icons/ListIcon'
+export { MoonIcon } from './icons/MoonIcon'
+export { RefreshIcon } from './icons/RefreshIcon'
+export { TrashBinIcon } from './icons/TrashBinIcon'
